eacbc78a5d3a43fb5d8241d7f729deee612c9f31
jarrodyellets <jarrod524@gmail.com>
2018-11-08 09:54:47 +0100

    Setup mLab

--- server/app.js
+++ server/app.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose.connect("mongodb://jarrod:test123@ds255253.mlab.com:55253/gql-books");
+mongoose.connection.once('open', () => {
+  console.log('connected to database');
+})
 
 app.use('/graphql', graphqlHTTP({
   schema,
