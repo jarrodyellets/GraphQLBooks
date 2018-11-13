c004bf4786431aa0489b1734d4ebc09d7a7b6565
jarrodyellets <jarrod524@gmail.com>
2018-11-13 14:13:37 +0100

    Added book query

--- client/src/components/BookList.js
+++ client/src/components/BookList.js
@@ -1,13 +1,38 @@
 import React, { Component } from 'react'
+import { gql } from 'apollo-boost';
+import { graphql } from 'react-apollo';
 
-export default class BookList extends Component {
+const getBooksQuery = gql`
+  {
+    books {
+      name
+      id
+    }
+  }
+`
+
+class BookList extends Component {
+  displayBooks(){
+    let data = this.props.data;
+    if(data.loading){
+      return(<div>Loading books...</div>)
+    } else {
+      return data.books.map(book => {
+        return(
+          <li key={book.id}>{book.name}</li>
+        )
+      })
+    }
+  }
   render() {
     return (
       <div>
         <ul id="book-list">
-          <li>Book name</li>
+          {this.displayBooks()}
         </ul>
       </div>
     )
   }
 }
+
+export default graphql(getBooksQuery)(BookList);
--- server/app.js
+++ server/app.js
@@ -2,8 +2,11 @@ const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const app = express();
+
+app.use(cors());
 
 mongoose.connect("mongodb://jarrod:test123@ds255253.mlab.com:55253/gql-books");
 mongoose.connection.once('open', () => {
